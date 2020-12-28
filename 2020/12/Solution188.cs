bbcb6d3d95774de1e8e3f77fc469e3831f22882e
CyanWind97 <916935477@qq.com>
2020-12-28 18:02:39 +0800

    No.188 Accept 208/211

--- 2020/12/Solution188.cs
+++ 2020/12/Solution188.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.IO;
+using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -102,13 +105,99 @@ namespace LeetCodeNote
             int count = buys.Count;
 
             if(k < count){
-                int[] remove = new int[k - count];
-                for(int i = 0; i < k - count; i++){
+                int[] delta = new int[count];
+                int[] type = new int[count];
+                for(int i = 1; i < count; i++){
+                    if(sells[i] > sells[i - 1] && buys[i] >= buys[i - 1]){
+                        delta[i] = sells[i - 1] - buys[i];
+                        type[i] = 1;
+                    }else if(sells[i] <= sells[i - 1] 
+                        && sells[i] - buys[i] < sells[i - 1] - buys[i - 1]){
+                        delta[i] = sells[i] - buys[i];
+                    }else{
+                        if(delta[i - 1] == 0 || sells[i - 1] - buys[i - 1] < delta[i - 1]){
+                            delta[i] = 0;
+                            delta[i - 1] = sells[i - 1] - buys[i - 1];
+                            type[i - 1] = 0;
+                        }
+                    }
+                }
+                while(count > k){
+                    int min = 0;
+                    int index = 0;
+                    for(int j = 0; j < count; j++){
+                        if(delta[j] > 0){
+                            if(delta[j] < min || min == 0){
+                                min = delta[j];
+                                index = j;
+                            }
+                        }
+                    }
+                    
+                    int tmpType = type[index];
+                    for(int j = index; j < count - 1; j++){
+                        delta[j] = delta[j + 1];
+                        type[j] = type[j + 1];
+                    }
+                    
+                    buys.RemoveAt(index);
+                    int maxIndex = index;
+                    if(tmpType == 1){
+                        index--;
+                        delta[index] = 0;
+                    }
+                    sells.RemoveAt(index);
+                    
+                    count--;
+                    if(k == count || index == 0){
+                        delta[index] = 0;
+                        continue;
+                    }
 
+                    for(int j = index; j <= count - 1 && j <= maxIndex; j++){
+                        if(sells[j] > sells[j - 1] && buys[j] >= buys[j - 1]){
+                            delta[j] = sells[j - 1] - buys[j];
+                            type[j] = 1;
+                        }else if(sells[j] <= sells[j - 1] 
+                            && sells[j] - buys[j] < sells[j - 1] - buys[j - 1]){
+                            delta[j] = sells[j] - buys[j];
+                            type[j] = 0;
+                        }else{
+                            if(delta[j - 1] == 0 || sells[j - 1] - buys[j - 1] < delta[j - 1]){
+                                delta[j] = 0;
+                                type[j] = 0;
+                                delta[j - 1] = sells[j - 1] - buys[j - 1];
+                                type[j - 1] = 0;
+                            }
+                        }
+                    }
                 }
             }
 
             return sells.Sum() - buys.Sum();
         }
+    
+        public static void WriteText(string tag, int count, int index, List<int> buys, List<int> sells, int[] delta, int[] type){
+            string path = @"test.txt";
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine();
+            sb.AppendLine($"{count}\t{tag}:");
+            sb.AppendLine($"index:\t{index}");
+            StringBuilder b = new StringBuilder("buy:");
+            StringBuilder s = new StringBuilder("sell:");
+            StringBuilder d = new StringBuilder("delta:");
+            StringBuilder t = new StringBuilder("type:");
+
+            int minIndex = index - 2 > 0 ? index - 2 : 0;
+            int maxIndex =  index + 2 < buys.Count() - 1 ? index + 2 : buys.Count() - 1;
+            for(int i = minIndex; i <= maxIndex; i++){
+                b.Append($"\t{buys[i]}");
+                s.Append($"\t{sells[i]}");
+                d.Append($"\t{delta[i]}");
+                t.Append($"\t{type[i]}");
+            }
+            sb.AppendLine(b.ToString()).AppendLine(s.ToString()).AppendLine(d.ToString()).AppendLine(t.ToString());
+            File.AppendAllText(path,sb.ToString());
+        } 
     }
 }
