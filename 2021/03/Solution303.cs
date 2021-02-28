726836a859f8f4ae6b283095f3fe3c175e409326
CyanWind97 <916935477@qq.com>
2021-03-01 00:19:29 +0800

    No.303 20210301

---
+++ 2021/03/Solution303.cs
@@ -0,0 +1,29 @@
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 303
+    /// title: 区域和检索 - 数组不可变
+    /// problems: https://leetcode-cn.com/problems/range-sum-query-immutable/
+    /// date: 20210301
+    /// </summary>
+    public static class Solution303
+    {
+        public class NumArray {
+
+            private int[] sums;
+
+            public NumArray(int[] nums) {
+                int length = nums.Length;
+                sums = new int[length + 1];
+                sums[0] = 0;
+                for(int i = 0; i < length; i++){
+                    sums[i + 1] = sums[i] + nums[i];
+                }
+            }
+            
+            public int SumRange(int i, int j) {
+                return sums[j + 1] - sums[i];
+            }
+        }
+    }
+}
