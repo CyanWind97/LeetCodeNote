55933e11a01be9c21d7353de70d7950da3e84731
CyanWind97 <916935477@qq.com>
2021-03-07 01:33:03 +0800

    No.131 20210307

---
+++ 2021/03/Solution131.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 131
+    /// title: 分割回文串
+    /// problems: https://leetcode-cn.com/problems/palindrome-partitioning/
+    /// date: 20210307
+    /// </summary>
+    public static class Solution131
+    {
+        // 参考解答 回溯法 记忆搜索法
+        public static IList<IList<string>> Partition(string s) {
+            int length = s.Length;
+            List<string> ans  = new List<string>();
+            List<IList<string>> result = new List<IList<string>>();
+            
+            int[,] f = new int[length,length];
+
+            int IsPalindrome(int i, int j){
+                if(f[i, j] == 0){
+                    if(i >= j)
+                        f[i, j] = 1;
+                    else if(s[i] == s[j])
+                        f[i, j] = IsPalindrome(i + 1, j - 1);
+                    else
+                        f[i, j] = -1;
+
+                }
+                
+                return f[i, j];
+
+            }
+
+            void dfs(int i){
+                if(i == length){
+                    result.Add(new List<string>(ans));
+                    return;
+                }
+                
+                for(int j = i; j < length; j++){
+                    if(IsPalindrome(i,j) == 1){
+                        ans.Add(s.Substring(i, j + 1 - i));
+                        dfs(j + 1);
+                        ans.RemoveAt(ans.Count() - 1);
+                    }
+                }
+            }
+
+            dfs(0);
+
+            return result;
+        }   
+    }
+}
