375c95d7ff5430f5afabd54edb3e76c2f66f85d5
CyanWind97 <916935477@qq.com>
2021-03-12 01:24:22 +0800

    No.331 20210312

---
+++ 2021/03/Solution331.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 331
+    /// title: 验证二叉树的前序序列化
+    /// problems: https://leetcode-cn.com/problems/verify-preorder-serialization-of-a-binary-tree/
+    /// date: 20210312
+    /// </summary>
+    public static class Solution331
+    {
+        public static bool IsValidSerialization(string preorder) {
+            int slots = 1;
+            int i = 0;
+            int length = preorder.Length;
+            while(i < length){
+                if(slots == 0)
+                    return false;
+                
+                if(preorder[i] == ',')
+                    i++;
+                else if(preorder[i] == '#'){
+                    slots--;
+                    i++;
+                }else{
+                    while(i < length && preorder[i] != ','){
+                        i++;
+                    }
+                    slots++;
+                }
+                    
+            }
+
+            return slots == 0;
+        }
+    }
+}
