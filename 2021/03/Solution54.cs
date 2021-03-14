89859de2a1706e7b1d799d5d1f2b9918586d7aaa
CyanWind97 <916935477@qq.com>
2021-03-15 01:13:53 +0800

    No.54 20210315

---
+++ 2021/03/Solution54.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+
+namespace LeetCodeNote
+{
+    public static class Solution54
+    {
+        public static IList<int> SpiralOrder(int[][] matrix) {
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+            int[] result = new int[m * n];
+
+            int left = 0, right = n - 1, top = 0, bottom = m - 1;
+            int index = 0;
+
+            while(left <= right && top <= bottom){
+                for(int j = left; j <= right; j++){
+                    result[index++] = matrix[top][j];
+                }
+
+                for(int i = top + 1;  i <= bottom; i++){
+                    result[index++] = matrix[i][right];
+                }
+
+                if(left < right && top < bottom){
+                    for(int j = right - 1; j > left; j--){
+                        result[index++] = matrix[bottom][j];
+                    }
+
+                    for(int i =  bottom;  i > top; i--){
+                        result[index++] = matrix[i][left];
+                    }
+                }
+                left++;
+                right--;
+                top++;
+                bottom--;
+            }
+
+            return result;
+        }
+    }
+}
