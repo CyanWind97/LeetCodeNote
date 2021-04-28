81462138dcf42cdadaa559bb3324c0eb00af6733
CyanWind97 <916935477@qq.com>
2021-04-28 17:50:17 +0800

    No.633 20210428

---
+++ 2021/04/Solution633.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 633
+    /// title: 扰乱字符串
+    /// problems: https://leetcode-cn.com/problems/sum-of-square-numbers/
+    /// date: 20210428
+    /// </summary>
+    public static class Solution633
+    {
+        public static bool JudgeSquareSum(int c) {
+
+            for(long a = 0; a * a <= c; a++){
+                double b = Math.Sqrt(c - a * a);
+                if(b == (int)b)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
