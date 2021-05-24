754d27df3996f0dc40270cb76efe95f58ec0ab78
Cyanwind97 <916935477@qq.com>
2021-05-24 17:49:21 +0800

    No.664 Todo

---
+++ 2021/05/Solution664.cs
@@ -0,0 +1,16 @@
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 664
+    /// title: 奇怪的打印机
+    /// problems: https://leetcode-cn.com/problems/strange-printer/
+    /// date: 20210524
+    /// </summary>
+    public static class Solution664
+    {
+        public static int StrangePrinter(string s) {
+
+            return 0;
+        }
+    }
+}
