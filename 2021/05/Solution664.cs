ec5db5894bfe7673df8755b32ef90d858959d527
CyanWind97 <916935477@qq.com>
2021-05-24 19:03:35 +0800

    No.664 20210524

--- 2021/05/Solution664.cs
+++ 2021/05/Solution664.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace LeetCodeNote
 {
     /// <summary>
@@ -9,8 +11,24 @@ namespace LeetCodeNote
     public static class Solution664
     {
         public static int StrangePrinter(string s) {
-
-            return 0;
+            int n = s.Length;
+            int[,] f = new int[n, n];
+            for (int i = n - 1; i >= 0; i--) {
+                f[i, i] = 1;
+                for (int j = i + 1; j < n; j++) {
+                    if (s[i] == s[j]) {
+                        f[i, j] = f[i, j - 1];
+                    } else {
+                        int minn = int.MaxValue;
+                        for (int k = i; k < j; k++) {
+                            minn = Math.Min(minn, f[i, k] + f[k + 1, j]);
+                        }
+                        f[i, j] = minn;
+                    }
+                }
+            }
+            
+            return f[0, n - 1];
         }
     }
 }
