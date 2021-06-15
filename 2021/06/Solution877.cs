e42d7ab1c6220ec793837481a01503333e1e9ee2
CyanWind97 <916935477@qq.com>
2021-06-16 01:15:40 +0800

    No.877 20210616

---
+++ 2021/06/Solution877.cs
@@ -0,0 +1,15 @@
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 877
+    /// title: 石子游戏
+    /// problems: https://leetcode-cn.com/problems/stone-game/
+    /// date: 20210616
+    /// </summary>
+    public static class Solution877
+    {
+        public static bool StoneGame(int[] piles) {
+            return true;
+        }
+    }
+}
