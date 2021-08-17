09f541bc23fbc40a95f565aa96c75901318bdc54
Cyanwind97 <916935477@qq.com>
2021-08-17 15:25:26 +0800

    No.551 20210817

---
+++ 2021/08/Solution551.cs
@@ -0,0 +1,35 @@
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 551
+    /// title: 学生出勤记录 I
+    /// problems: https://leetcode-cn.com/problems/student-attendance-record-i/
+    /// date: 20210817
+    /// </summary>
+    public static class Solution551
+    {
+
+        public static bool CheckRecord(string s) {
+            int ACount = 0;
+            int LCount = 0;
+            foreach(var c in s){
+                if(c == 'L'){
+                    LCount++;
+                    if(LCount >= 3)
+                        return false;
+                }else{
+                    LCount = 0;
+
+                    if(c == 'A'){
+                        ACount++;
+
+                        if(ACount >= 2)
+                            return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
