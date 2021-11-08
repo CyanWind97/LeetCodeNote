e00687514c35a37e280f69cd28f8410e572fce48
Cyanwind97 <916935477@qq.com>
2021-11-08 16:03:41 +0800

    No.299 20211108

---
+++ 2021/11/Solution299.cs
@@ -0,0 +1,38 @@
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 299
+    /// title:  猜数字游戏
+    /// problems: https://leetcode-cn.com/problems/bulls-and-cows/
+    /// date: 20211108
+    /// </summary>
+    public static class Solution299
+    {
+        public static string GetHint(string secret, string guess) {
+            int aCount = 0;
+            int bCount = 0;
+
+            int[] counts = new int[10];
+            
+            int length = secret.Length;
+            for(int i = 0; i < length; i++){
+                if (secret[i] == guess[i]){
+                    aCount++;
+                    continue;
+                }
+
+                if (counts[secret[i] - '0'] < 0)
+                    bCount++;
+                
+                counts[secret[i] - '0']++;
+
+                if (counts[guess[i] - '0'] > 0)
+                    bCount++;
+                
+                counts[guess[i] - '0']--;
+            }
+
+            return $"{aCount}A{bCount}B";
+        }
+    }
+}
