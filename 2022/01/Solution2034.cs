bb771201167dcf0c7bdd9369273ff9d8448becd4
CyanWind97 <916935477@qq.com>
2022-01-23 21:23:04 +0800

    No.2034 20220123

---
+++ 2022/01/Solution2034.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 2034
+    /// title: 股票价格波动
+    /// problems: https://leetcode-cn.com/problems/stock-price-fluctuation/
+    /// date: 20220123
+    /// </summary>
+    public static class Solution2034
+    {
+        public class StockPrice {
+            Dictionary<int, int> time2price = new Dictionary<int, int>();
+            SortedSet<int> set = new SortedSet<int>();
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int latestTime = -1;
+
+            public StockPrice() {
+
+            }
+            
+            public void Update(int timestamp, int price) {
+                set.Add(price);
+
+                if (dic.ContainsKey(price))
+                    ++dic[price];
+                else
+                    dic[price] = 1;
+
+                if (time2price.ContainsKey(timestamp))
+                {
+                    int val = time2price[timestamp];
+                    --dic[val];
+
+                    if (dic[val] == 0)
+                    {
+                        dic.Remove(val);
+                        set.Remove(val);
+                    }
+                }
+
+                time2price[timestamp] = price;
+                latestTime = Math.Max(latestTime, timestamp);
+            }
+            
+            public int Current() {
+                if (latestTime >= 0)
+                    return time2price[latestTime];
+                
+                return -1;
+            }
+            
+            public int Maximum() {
+                return set.Max;
+            }
+            
+            public int Minimum() {
+                return set.Min;
+            }
+        }
+
+    }
+}
