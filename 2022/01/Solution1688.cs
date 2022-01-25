a60d17473cec2c897194c527dc145e7dd0186728
CyanWind97 <916935477@qq.com>
2022-01-25 14:16:31 +0800

    No.1688 20220125

---
+++ 2022/01/Solution1688.cs
@@ -0,0 +1,15 @@
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 1688
+    /// title: 比赛中的配对次数
+    /// problems: https://leetcode-cn.com/problems/count-of-matches-in-tournament/
+    /// date: 20220125
+    /// </summary>
+    public static class Solution1688
+    {
+        public static int NumberOfMatches(int n) {
+            return n - 1;
+        }
+    }
+}
