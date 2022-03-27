07e9f8fad24d2bcd926c5cfd7eb171999bb14aa7
CyanWind97 <916935477@qq.com>
2022-03-27 15:05:23 +0800

    No.2028 20220327

---
+++ 2022/03/Solution2028.cs
@@ -0,0 +1,32 @@
+using System.Linq;
+
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 2028
+    /// title: 找出缺失的观测数据
+    /// problems: https://leetcode-cn.com/problems/find-missing-observations/
+    /// date: 20220327
+    /// </summary>
+
+    public static class Solution2028
+    {
+        public static int[] MissingRolls(int[] rolls, int mean, int n) {
+            int m = rolls.Length;
+            int sum = mean * (m + n);
+            sum -= rolls.Sum();
+            
+            if(sum < n || sum > 6 * n)
+                return new int[]{};
+
+            int quotient = sum / n;
+            int remainder = sum % n;
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = quotient + (i < remainder ? 1 : 0);
+            }
+
+            return result;
+        }
+    }
+}
