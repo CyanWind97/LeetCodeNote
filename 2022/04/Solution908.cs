c59980c367f6c3f1cc4681270edcd8048a648085
CyanWind97 <916935477@qq.com>
2022-04-30 19:21:54 +0800

    No.908 20220430

---
+++ 2022/04/Solution908.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 908
+    /// title: 最小差值 I
+    /// problems: https://leetcode-cn.com/problems/smallest-range-i/
+    /// date: 20220430
+    /// </summary>
+    public static class Solution908
+    {
+        public static int SmallestRangeI(int[] nums, int k) {
+            int length = nums.Length;
+            int min = 10001;
+            int max = -1;
+
+            foreach(var num in nums){
+                min = Math.Min(num, min);
+                max = Math.Max(num, max);
+            }
+
+
+            return max - min > 2 * k  ? max - min - 2 * k : 0;
+        }
+    }
+}
