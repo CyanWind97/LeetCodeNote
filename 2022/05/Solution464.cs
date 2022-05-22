2254f5c4afc8d93db5eb3b9981747e7eb2c8758b
CyanWind97 <916935477@qq.com>
2022-05-22 14:25:42 +0800

    No.464 20220522

---
+++ 2022/05/Solution464.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 464
+    /// title: 我能赢吗
+    /// problems: https://leetcode.cn/problems/can-i-win/
+    /// date: 20220522
+    /// </summary>  
+    public static class Solution464
+    {
+        // 参考解答 记忆搜索 + 状态压缩
+        public static bool CanIWin(int maxChoosableInteger, int desiredTotal) {
+            int max = maxChoosableInteger * (maxChoosableInteger + 1) / 2;
+            if(max < desiredTotal)
+                return false;
+            
+            var memo = new Dictionary<int, bool>();
+
+            bool DFS(int usedNumbers, int current){
+                if(!memo.ContainsKey(usedNumbers)){
+                    bool result = false;
+                    for(int i = 0; i < maxChoosableInteger; i++){
+                        if(((usedNumbers >> i) & 1) == 0){
+                            if(i + 1 + current >= desiredTotal){
+                                result = true;
+                                break;
+                            }
+
+                            if(!DFS(usedNumbers | (1 << i), current + usedNumbers + i + 1)){
+                                result = true;
+                                break;
+                            }
+                        }
+
+                        memo.Add(usedNumbers, result);
+                    }
+                }
+
+                return memo[usedNumbers];
+            }
+
+            return DFS(0, 0);
+        }
+    }
+}
