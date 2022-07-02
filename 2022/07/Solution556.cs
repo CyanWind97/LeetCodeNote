c310f250ee428f3a4bc654b87254bf2119fe3eeb
CyanWind97 <916935477@qq.com>
2022-07-03 02:29:39 +0800

    No.556 20220703

---
+++ 2022/07/Solution556.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 556
+    /// title: 下一个更大元素 III
+    /// problems: https://leetcode.cn/problems/next-greater-element-iii/
+    /// date: 202200703
+    /// </summary>
+    public static class Solution556
+    {
+        public static int NextGreaterElement(int n) {
+            var nums = n.ToString().ToCharArray();
+            int length = nums.Length;
+            int i = length - 2;
+
+            while(i >= 0 && nums[i] >= nums[i + 1]){
+                i--;
+            }
+
+            if(i < 0)
+                return -1;
+
+            int j = length - 1;
+            while(j >= 0 && nums[i] >= nums[j]){
+                j--;
+            }
+
+            (nums[i], nums[j]) = (nums[j], nums[i]);
+            Array.Reverse(nums, i + 1, length - i - 1);
+
+            var result = long.Parse(new string(nums));
+
+            return result > int.MaxValue ? -1 : (int)result;
+        }
+    }
+}
