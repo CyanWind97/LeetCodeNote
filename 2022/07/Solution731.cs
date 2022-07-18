9d15bb15a3b66a2069ca1e79e985e3d0c34451bc
CyanWind97 <916935477@qq.com>
2022-07-19 00:06:09 +0800

    No.731 20220719

---
+++ 2022/07/Solution731.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 731
+    /// title: 我的日程安排表  II
+    /// problems: https://leetcode.cn/problems/my-calendar-ii/
+    /// date: 20220719
+    /// </summary>
+
+    public static class Solution731
+    {
+        public class MyCalendarTwo {
+            List<(int Start, int End)> booked;
+            List<(int Start, int End)> overlaps;
+
+            public MyCalendarTwo() {
+                booked = new ();
+                overlaps = new ();
+            }
+
+            public bool Book(int start, int end) {
+                foreach (var overlap in overlaps) {
+                    (int l, int r) = overlap;
+                    if (l < end && start < r) 
+                        return false;
+                }
+                foreach (var book in booked) {
+                    (int l, int r) = book;
+                    if (l < end && start < r) 
+                        overlaps.Add((Math.Max(l, start), Math.Min(r, end)));
+                }
+
+                booked.Add((start, end));
+                return true;
+            }
+        }
+    }
+}
