39b711110bf4dd0fedd35174405836c223d80d5b
CyanWind97 <916935477@qq.com>
2022-09-06 01:44:55 +0800

    No.828 20220906

---
+++ 2022/09/Solution828.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace LeetCodeNote
+{
+    // <summary>
+    /// no: 828
+    /// title: 统计子串中的唯一字符
+    /// problems: https://leetcode.cn/problems/count-unique-characters-of-all-substrings-of-a-given-string/
+    /// date: 20220906
+    /// </summary>
+    public static class Solution828
+    {
+        // 参考解答
+        public static int UniqueLetterString(string s) {
+            
+        }
+    }
+}
