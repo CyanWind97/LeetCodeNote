63d3d63dce0c4b81540290b63b03d7248ded3dda
CyanWind97 <916935477@qq.com>
2022-09-07 00:07:02 +0800

    No.1592 20220907

---
+++ 2022/09/Solution1592.cs
@@ -0,0 +1,55 @@
+using System.Text;
+
+namespace LeetCodeNote
+{
+    // <summary>
+    /// no: 1592
+    /// title: 重新排列单词间的空格
+    /// problems: https://leetcode.cn/problems/rearrange-spaces-between-words/
+    /// date: 20220907
+    /// </summary>
+    public static class Solution1592
+    {
+        public static string ReorderSpaces(string text) {
+            int length = text.Length;
+            string[] words = text.Trim().Split(" ");
+            int cntSpace = length;
+            int wordCount = 0;
+            foreach (string word in words) {
+                if (word.Length > 0) {
+                    cntSpace -= word.Length;
+                    wordCount++;
+                }
+            }
+            var sb = new StringBuilder();
+            if (words.Length == 1) {
+                sb.Append(words[0]);
+                for (int i = 0; i < cntSpace; i++) {
+                    sb.Append(' ');
+                }
+
+                return sb.ToString();
+            }
+
+            int perSpace = cntSpace / (wordCount - 1);
+            int restSpace = cntSpace % (wordCount - 1);
+            for (int i = 0; i < words.Length; i++) {
+                if (words[i].Length == 0) {
+                    continue;
+                }
+                if (sb.Length > 0) {
+                    for (int j = 0; j < perSpace; j++) {
+                        sb.Append(' ');
+                    }
+                }
+                sb.Append(words[i]);
+            }
+            
+            for (int i = 0; i < restSpace; i++) {
+                sb.Append(' ');
+            }
+
+            return sb.ToString();
+        }
+    }
+}
--- 2022/09/Solution828.cs
+++ 2022/09/Solution828.cs
@@ -12,7 +12,25 @@ namespace LeetCodeNote
     {
         // 参考解答
         public static int UniqueLetterString(string s) {
-            
+            var index = new Dictionary<char, IList<int>>();
+            for (int i = 0; i < s.Length; i++) {
+                if (!index.ContainsKey(s[i])) {
+                    index.Add(s[i], new List<int>());
+                    index[s[i]].Add(-1);
+                }
+
+                index[s[i]].Add(i);
+            }
+            int result = 0;
+            foreach (var pair in index) {
+                var arr = pair.Value;
+                arr.Add(s.Length);
+                for (int i = 1; i < arr.Count - 1; i++) {
+                    result += (arr[i] - arr[i - 1]) * (arr[i + 1] - arr[i]);
+                }
+            }
+
+            return result;
         }
     }
 }
