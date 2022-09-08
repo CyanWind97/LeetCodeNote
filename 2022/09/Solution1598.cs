465eb3afd009ecfc8825cdee1918a8f9d3399ca6
CyanWind97 <916935477@qq.com>
2022-09-09 00:09:02 +0800

    No.1598 20220909

---
+++ 2022/09/Solution1598.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+namespace LeetCodeNote
+{
+    // <summary>
+    /// no: 1598
+    /// title: 文件夹操作日志搜集器
+    /// problems: https://leetcode.cn/problems/crawler-log-folder/
+    /// date: 20220909
+    /// </summary>
+
+    public static class Solution1598
+    {
+        public static int MinOperations(string[] logs) {
+            int operations = 0;
+            
+            foreach(var log in logs){
+                if(log == "../"){
+                    if(operations > 0)
+                        operations--;
+                }else if(log == "./"){
+                    continue;
+                }else{
+                    operations++;
+                }
+            }
+
+            return operations;
+        }   
+    }
+}
