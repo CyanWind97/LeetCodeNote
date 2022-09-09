6a09ee5a699869c3f6d8c001f2e3d43e90c638f5
CyanWind97 <916935477@qq.com>
2022-09-10 00:56:05 +0800

    No.669 20220910

---
+++ 2022/09/Solution669.cs
@@ -0,0 +1,47 @@
+namespace LeetCodeNote
+{
+    // <summary>
+    /// no: 669
+    /// title: 修剪二叉搜索树
+    /// problems: https://leetcode.cn/problems/trim-a-binary-search-tree/
+    /// date: 20220910
+    /// </summary>
+    public static class Solution669
+    {
+        public class TreeNode {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+
+        public static TreeNode TrimBST(TreeNode root, int low, int high) {
+            while (root != null && (root.val < low || root.val > high)) {
+                    root = root.val < low ? root.right : root = root.left;
+            }
+
+            if (root == null) 
+                return null;
+            
+            for (var node = root; node.left != null; ) {
+                if (node.left.val < low)
+                    node.left = node.left.right;
+                else
+                    node = node.left;
+            }
+            for (var node = root; node.right != null; ) {
+                if (node.right.val > high)
+                    node.right = node.right.left;
+                else
+                    node = node.right;
+            }
+
+            return root;
+        }
+    }
+}
