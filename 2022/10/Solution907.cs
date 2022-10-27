b02484cbd568ffeb2d1bcf687b0c2b95d3807a02
CyanWind97 <916935477@qq.com>
2022-10-28 00:25:19 +0800

    No.907 20221028

---
+++ 2022/10/Solution907.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 907
+    /// title: 子数组的最小值之和
+    /// problems: https://leetcode.cn/problems/sum-of-subarray-minimums/
+    /// date: 20221028
+    /// </summary>    
+    public static class Solution907
+    {
+        public static int SumSubarrayMins(int[] arr) {
+            int MOD = 1_000_000_007;
+
+            var stack = new Stack<(int Value, int Count)>();
+            int result = 0;
+            int dot = 0;
+
+            int length= arr.Length;
+            for(int j = 0; j < length; j++){
+                int count = 1;
+                int num = arr[j];
+                while(stack.Count > 0 && stack.Peek().Value >= num){
+                    var top = stack.Pop();
+                    count += top.Count;
+                    dot -= top.Value * top.Count;                    
+                }
+
+                stack.Push((num, count));
+                dot += num * count;
+                result += dot;
+                result %= MOD;
+            }
+            
+
+            return result;
+        }
+    }
+}
