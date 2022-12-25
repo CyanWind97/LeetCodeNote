a48977186da7f2a81d194aacc30d9d332b3d3704
CyanWind97 <916935477@qq.com>
2022-12-26 00:04:21 +0800

    No.1759

---
+++ 2022/12/Solution1759.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 1759
+    /// title: 放置盒子
+    /// problems: https://leetcode.cn/problems/count-number-of-homogenous-substrings/
+    /// date: 20221226
+    /// </summary>
+    public static class Solution1759
+    {
+        // 参考解答
+        public static int CountHomogenous(string s) {
+            const int MOD = 1000000007;
+            long res = 0;
+            char prev = s[0];
+            int cnt = 0;
+            foreach (char c in s) {
+                if (c == prev) {
+                    cnt++;
+                } else {
+                    res += (long) (cnt + 1) * cnt / 2;
+                    cnt = 1;
+                    prev = c;
+                }
+            }
+            res += (long) (cnt + 1) * cnt / 2;
+            return (int) (res % MOD);
+        }
+    }
+}
