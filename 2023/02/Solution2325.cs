b651215b14d09d79aa9b6f4085e338365a200c50
CyanWind97 <916935477@qq.com>
2023-02-01 00:20:03 +0800

    No.2325 20230201

---
+++ 2023/02/Solution2325.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCodeNote
+{
+    // <summary>
+    /// no: 2325
+    /// title: 解密消息 X 矩阵
+    /// problems: https://leetcode.cn/problems/decode-the-message/
+    /// date: 20230201
+    /// </summary>
+    public static class Solution2325
+    {
+        public static string DecodeMessage(string key, string message) {
+            var map = key.ToHashSet()
+                        .Where(c => char.IsLower(c))
+                        .Select((c, i) => ((char)(i + 'a'), c))
+                        .ToDictionary(p => p.c, p => p.Item1);
+            map.Add(' ', ' ');
+
+            var chars = message.Select(c => map[c]).ToArray();
+
+            return new string(chars);
+        }
+    }
+}
