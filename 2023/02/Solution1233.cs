f46387083acfbd282517b0b9f9828e549c4c1ce7
CyanWind97 <916935477@qq.com>
2023-02-08 00:30:35 +0800

    No.1233 20230208

---
+++ 2023/02/Solution1233.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCodeNote
+{
+    // <summary>
+    /// no: 1233
+    /// title: 删除子文件夹
+    /// problems: https://leetcode.cn/problems/remove-sub-folders-from-the-filesystem/
+    /// date: 20230208
+    /// </summary>
+    public static class Solution1233
+    {
+        public static IList<string> RemoveSubfolders(string[] folder) {
+            Array.Sort(folder);
+            var result = new List<string>();
+            int length = folder.Length;
+            result.Add(folder[0]);
+            for(int i = 1; i < length; i++){
+                var pre = result.Last();
+                
+                if(!(pre.Length < folder[i].Length
+                    && folder[i].StartsWith(pre)
+                    && folder[i][pre.Length] == '/'))
+                    result.Add(folder[i]);
+            }
+
+            return result;
+        }
+    }
+}
