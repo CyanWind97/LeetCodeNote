5c81b51e9f1f3e8897c90f9ac3267a149ccef7f5
CyanWind97 <916935477@qq.com>
2023-06-27 00:36:18 +0800

    No.1186 20230627

---
+++ 2023/06/Solution1186.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 1186
+    /// title: 删除一次得到子数组最大和
+    /// problems: https://leetcode.cn/problems/maximum-subarray-sum-with-one-deletion/
+    /// date: 20230627
+    /// </summary>
+    public static class Solution1186
+    {
+        public static int MaximumSum(int[] arr) {
+            int length = arr.Length;
+            int max = arr[0];
+            int dp0 = arr[0];
+            int dp1 = 0;
+
+            for(int i = 1; i < length; i++){
+                dp1 = Math.Max(dp0, dp1 + arr[i]);
+                dp0 = Math.Max(dp0, 0) + arr[i];
+                max = Math.Max(max, Math.Max(dp0, dp1));
+            }
+
+            return max;
+        }
+    }
+}
