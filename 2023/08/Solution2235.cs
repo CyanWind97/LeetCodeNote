286f5e58fe61fb168be4e2f1f8bed6b2e6cc1ceb
CyanWind97 <916935477@qq.com>
2023-08-19 00:12:13 +0800

    No.2235 20230819

---
+++ 2023/08/Solution2235.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 2235
+    /// title: 两整数相加
+    /// problems: https://leetcode.cn/problems/add-two-integers/
+    /// date: 20230819
+    /// </summary>
+    public class Solution2235
+    {
+        public static int Sum(int num1, int num2) {
+            return num1 + num2;
+        }
+    }
+}
