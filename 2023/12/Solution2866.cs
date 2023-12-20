7dd3d0ea27edd31febe58f978372628f5daff165
CyanWind97 <916935477@qq.com>
2023-12-21 00:38:35 +0800

    No.2866 20231221

---
+++ 2023/12/Solution2866.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCodeNote
+{
+    /// <summary>
+    /// no: 2866
+    /// title: 美丽塔 II
+    /// problems: https://leetcode.cn/problems/beautiful-towers-ii/description/?envType=daily-question&envId=2023-12-21
+    /// date: 20231221
+    /// </summary>
+    public static class Solution2866
+    {
+        // 参考解答
+        // 单调栈 + dp
+        public static long MaximumSumOfHeights(IList<int> maxHeights) {
+            var n = maxHeights.Count;
+            long result = 0;
+            var prefix = new long[n];
+            var suffix = new long[n];
+            var stack1 = new Stack<int>();
+            var stack2 = new Stack<int>();
+
+            for(int i = 0; i < n; i++){
+                while(stack1.Count > 0 && maxHeights[i] < maxHeights[stack1.Peek()])
+                    stack1.Pop();
+                
+                prefix[i] = stack1.Count == 0 
+                        ? (long)(i + 1) * maxHeights[i]
+                        : prefix[stack1.Peek()] + (i - stack1.Peek()) * (long)maxHeights[i];
+                
+                stack1.Push(i);
+            }
+
+            for(int i = n - 1; i >= 0; i--){
+                while(stack2.Count > 0 && maxHeights[i] < maxHeights[stack2.Peek()])
+                    stack2.Pop();
+                
+                suffix[i] = stack2.Count == 0 
+                        ? (long)(n - i) * maxHeights[i]
+                        : suffix[stack2.Peek()] + (stack2.Peek() - i) * (long)maxHeights[i];
+                
+                stack2.Push(i);
+                result = Math.Max(result, prefix[i] + suffix[i] - maxHeights[i]);
+            }
+
+            return result;
+        }
+    }
+}
