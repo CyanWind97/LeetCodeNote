d6f6563c5202ff75f5434b358a81e13b94ddcc8b
CyanWind97 <916935477@qq.com>
2024-04-01 06:34:04 +0800

    修改目录结构，增加测试用项目

---
+++ test/LeetCodeNote.Test/2024/Test/Test2810.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeNote.Test._2024;
+
+[TestClass]
+public class Test2810
+{
+    [TestMethod]
+    [DataRow("string", "rtsng")]
+    [DataRow("poiinter", "ponter")]
+    [DataRow("viwif", "wvf")]
+    public void Test1(string s, string expected)
+    {
+        var result = Solution2810.FinalString(s);
+        Assert.AreEqual(expected, result);
+    }
+}
