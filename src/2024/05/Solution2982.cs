dfd22edaa1e399b2e28664a4bf4e7c2cf5d87008
CyanWind97 <916935477@qq.com>
2024-05-30 01:57:54 +0800

    No.2982 20240530

---
+++ src/2024/05/Solution2982.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCodeNote;
+
+/// <summary>
+/// no: 2982
+/// title: 找出出现至少三次的最长特殊子字符串 II
+/// problems: https://leetcode.cn/problems/find-longest-special-substring-that-occurs-thrice-ii
+/// date: 20240530
+/// </summary>
+public static class Solution2982
+{
+    public static int MaximumLength(string s) {
+        int length = s.Length;
+        var counts = new (int Max1, int Max2, int Max3)[26];
+
+        int count = 0;
+        for(int i = 0; i < length; i++){
+            count++;
+            if (!(i + 1 == length || s[i] != s[i + 1]))
+                continue;
+            
+            int ch = s[i] - 'a';
+            if (count > counts[ch].Max1)
+                counts[ch] = (count, counts[ch].Max1, counts[ch].Max2);
+            else if(count > counts[ch].Max2)
+                counts[ch] = (counts[ch].Max1, count, counts[ch].Max2);
+            else if(count > counts[ch].Max3)
+                counts[ch] = (counts[ch].Max1, counts[ch].Max2, count);
+
+            count = 0;
+        }
+        
+
+        int result = -1;
+        foreach(var (max1, max2, max3) in counts){
+            if (max1 > 2)
+                result = Math.Max(result, max1 - 2);
+
+            if (max2 > 0 && max1 > 1)
+                result = Math.Max(result, Math.Min(max1  - 1, max2));
+            
+            if (max3 > 0)
+                result = Math.Max(result, max3);
+        }
+
+        return result;
+    }
+}
