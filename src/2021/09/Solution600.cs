0389ea5a009c30777078ec1dfe0ad7ec31179403
CyanWind97 <916935477@qq.com>
2024-08-05 00:30:29 +0800

    No.600 20240805

--- src/2021/09/Solution600.cs
+++ src/2021/09/Solution600.cs
@@ -7,35 +7,7 @@ namespace LeetCodeNote
     /// date: 20210911
     /// </summary>
 
-    public static class Solution600
+    public static partial class Solution600
     {
-        // 参考解答动态规划
-        public static int FindIntegers(int n) {
-            int[] dp = new int[31];
-            dp[0] = dp[1] = 1;
-
-            for (int i = 2; i < 31; ++i) {
-                dp[i] = dp[i - 1] + dp[i - 2];
-            }
-
-            int pre = 0, res = 0;
-            for (int i = 29; i >= 0; --i) {
-                int val = 1 << i;
-                if ((n & val) != 0) {
-                    res += dp[i + 1];
-                    if (pre == 1)
-                        break;
-                    
-                    pre = 1;
-                } else {
-                    pre = 0;
-                }
-
-                if (i == 0)
-                    ++res;
-            }
-
-            return res;
-        }
     }
 }
---
+++ src/2024/08/Solution600.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeetCodeNote;
+
+/// <summary>
+/// no: 600
+/// title: 不含连续1的非负整数
+/// problems: https://leetcode-cn.com/problems/non-negative-integers-without-consecutive-ones/
+/// date: 20240805
+/// </summary>
+public static partial class Solution600
+{
+    // 参考解答 动态规划
+    public static int FindIntegers(int n) {
+        int[] dp = new int[31];
+        dp[0] = dp[1] = 1;
+
+        for (int i = 2; i < 31; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        int pre = 0, res = 0;
+        for (int i = 29; i >= 0; --i) {
+            int val = 1 << i;
+            if ((n & val) != 0) {
+                res += dp[i + 1];
+                if (pre == 1)
+                    break;
+                
+                pre = 1;
+            } else
+                pre = 0;
+
+            if (i == 0)
+                ++res;
+        }
+
+        return res;
+    }
+}
