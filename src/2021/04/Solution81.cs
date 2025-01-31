d55922c89b49e348ca496ece6e2b73b86cb192f0
CyanWind97 <916935477@qq.com>
2025-02-01 00:03:18 +0800

    No.81 20250201

--- src/2021/04/Solution81.cs
+++ src/2021/04/Solution81.cs
@@ -6,43 +6,7 @@ namespace LeetCodeNote
     /// problems: https://leetcode-cn.com/problems/search-in-rotated-sorted-array-ii/
     /// date: 20210407
     /// </summary>
-    public static class Solution81
+    public static partial class Solution81
     {
-        public static bool Search(int[] nums, int target) {
-            int length = nums.Length;
-            if(length == 1)
-                return target == nums[0];
-            
-            int left = 0;
-            int right = length - 1; 
-            while(left <= right)
-            {
-                int mid = (left + right) / 2;
-                
-                if(target == nums[mid])
-                    return true;
-                
-                if (nums[left] == nums[mid] && nums[mid] == nums[right]) 
-                {
-                    ++left;
-                    --right;
-                }else if(nums[left] <= nums[mid])
-                {
-                    if (nums[left] <= target && target < nums[mid])
-                        right = mid - 1;
-                    else
-                        left = mid + 1;
-                    
-                } else {
-                    if (nums[mid] < target && target <= nums[length - 1])
-                        left = mid + 1;
-                    else
-                        right = mid - 1;
-                }
-                
-            }
-
-            return false;
-        }
     }
 }
